1f7cd00fa33ee4253c2d82029ff648eabebc88a3
Philip Lee <leephili@usc.edu>
2014-10-25 23:06:07 -0700

    Overwriting with my stuff

---
+++ background.js
@@ -0,0 +1,105 @@
+var pageCount = 0;
+var currentTabId = null;
+var currentSite = null;
+var domain = /^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/;
+
+
+function getSiteFromUrl(url) {
+  var match = url.match(domain);
+  if (match) {
+    console.log(match[1]);
+    return match[1];
+  }
+  return null;
+}
+
+function updateTime(site, seconds) {
+  var loc = site.indexOf('twitter.com');
+  if(loc != -1){
+    localStorage['twitter']+= seconds;
+  }
+
+  loc = site.indexOf('facebook.com');
+  if(loc != -1){
+    localStorage['facebook']+= seconds;
+  }
+  console.log("Time: " + seconds);
+  // var sites = JSON.parse(localStorage.sites);
+  // if (!sites[site]) {
+  //   sites[site] = 0;
+  // }
+  // sites[site] = sites[site] + seconds;
+  // localStorage.sites = JSON.stringify(sites);
+}
+
+function updateCounter(){
+  var tabInt = 0;
+  if (currentTabId == null) {
+        return;
+      }
+
+      chrome.tabs.get(currentTabId, function(tab) {
+        /* Make sure we're on the focused window, otherwise we're recording bogus stats. */
+        chrome.windows.get( tab.windowId, function(window) {
+          if (!window.focused) {
+            return;
+          }
+          var site = getSiteFromUrl(tab.url);
+          if (site == null) {
+            console.log("Unable to update counter. Malformed url.");
+            return;
+          }
+
+          /* We can't update any counters if this is the first time visiting any
+           * site. This happens on browser startup. Initialize some variables so
+           * we can perform an update next time. */
+          if (currentSite == null) {
+            currentSite = site;
+            startTime = new Date();
+            return;
+          }
+
+          /* Update the time spent for this site by comparing the current time to
+           * the last time we were ran. */
+          var now = new Date();
+          var delta = now.getTime() - startTime.getTime();
+          updateTime(currentSite, delta/1000);
+
+          /* This function could have been called as the result of a tab change,
+           * which means the site may have changed. */
+          currentSite = site;
+          startTime = now;
+        });
+      });
+}
+
+function initialize(){
+  if (!localStorage.sites) {
+    localStorage.sites = JSON.stringify({});
+  }
+  localStorage['twitter'] = 0;
+  localStorage['facebook']=0;
+
+  chrome.tabs.onActivated.addListener(
+    function(tab) {
+      // console.log("Tab changed");
+      currentTabId = tab.tabId;
+      updateCounter();
+    });
+
+  chrome.windows.onFocusChanged.addListener(
+    function(windowId) {
+      // console.log("Detected window focus changed.");
+      chrome.tabs.getSelected(windowId,
+        function(tab) {
+          // console.log("Window/Tab changed");
+          currentTabId = tab.id;
+          updateCounter();
+        });
+    });
+
+}
+
+document.addEventListener('DOMContentLoaded', function () { 
+  initialize();
+});
--- popup.js
+++ popup.js
@@ -1,8 +1,11 @@
-chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-    var parser = document.createElement('a');
-    var url = tabs[0].url;
-    parser.href = url;
-    localStorage.setItem("URL", parser.href);
-    var value = localStorage.getItem("hello");
-    alert(value);
+document.addEventListener('DOMContentLoaded', function () { 
+    var sites = JSON.stringify(localStorage.sites);
+    console.log(sites);
+    var t = localStorage['twitter'];
+    var f = localStorage['facebook'];
+
+	// for(int i = 0; i++; i < sites.length){
+	// 	document.getElementById("stats").append(".");
+	// }
+	document.getElementById("stats").innerHTML = "T: " + t + "   F: " + f;
 });
